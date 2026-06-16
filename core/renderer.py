75284ca8c095497343e6909f6d4f8fa8cf9d4f08
Minitug <Minitug94@gmail.com>
2026-06-16 03:01:30 +0200

    Created very basic renderer

---
+++ core/renderer.py
@@ -0,0 +1,8 @@
+import pygame
+from core.car_env import CarEnv
+
+
+def draw(screen, env: CarEnv):
+    screen.fill((0, 0, 0))
+    for car in env.cars:
+        pygame.draw.rect(screen, (255, 0, 0), (car.position.x, car.position.y, car.LENGTH, car.WIDTH))
