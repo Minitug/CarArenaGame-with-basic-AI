ed0f30bee5042e348f075b53a4c65e7f639bd1e9
Minitug <Minitug94@gmail.com>
2026-06-16 02:23:58 +0200

    Updated position storage, added steering angle

--- core/car.py
+++ core/car.py
@@ -6,28 +6,28 @@ class Car:
     MAX_ACCELERATION = 240
     WIDTH = 190
     LENGTH = 340
-    MAX_STEERING_ANGLE = 1
+    MAX_STEERING_ANGLE = 35
     STEERING_SPEED = 120
     STEERING_RETURN_SPEED = 180
     TURN_RATE = 180
 
 
-    def __init__(self, x, y):
-        # self.x = x
-        # self.y = y
-        self.position = Vector2(x, y)
+    def __init__(self, position):
+        self.position = Vector2(position)
         self.speed = 0
         self.heading = 0
         self.throttle = 0
         self.steering = 0
+        self.steering_angle = 0
 
     def set_controls(self, throttle, steering):
         self.throttle = max(-1, min(1, throttle))
         self.steering = max(-1, min(1, steering))
 
-    def reset(self, x, y):
-        self.position = Vector2(x, y)
+    def reset(self, position):
+        self.position = Vector2(position)
         self.speed = 0
         self.heading = 0
         self.throttle = 0
         self.steering = 0
+        self.steering_angle = 0
