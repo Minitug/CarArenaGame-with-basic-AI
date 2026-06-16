70b9f244b174b71f7f3e9e99507f161d5979b62e
Minitug <Minitug94@gmail.com>
2026-06-16 02:36:29 +0200

    Basic car_env

---
+++ core/car_env.py
@@ -0,0 +1,23 @@
+from pygame.math import Vector2
+from core.car import Car
+
+class CarEnv:
+    def __init__(self, width, height, spawn, verbose=True, players = 1, max_cars = 4):
+        self.width = width
+        self.height = height
+        self.spawn = Vector2(spawn)
+        self.verbose = verbose
+        self.players = players
+        self.max_cars = max_cars
+        self.cars = self.create_cars()
+
+
+    def create_cars(self):
+        cars = []
+        for i in range(self.max_cars):
+            cars.append(Car(position=self.spawn))
+        return cars
+
+    def reset(self):
+        for car in self.cars:
+            car.reset(position=self.spawn)
